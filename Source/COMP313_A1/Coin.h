605aeb9216d7568f3b8f3e2c84213389afbf1ee6
Raine <legaspi.raine@yahoo.com>
2020-08-08 17:56:29 +1200

    project files added

---
+++ Source/COMP313_A1/Coin.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Coin.generated.h"
+
+UCLASS()
+class COMP313_A1_API ACoin : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACoin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
